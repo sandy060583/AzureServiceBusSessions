23c7f16c70ba7f53b98d47193168beee28580726
Sandeep Ramani <sandeep.ramani@dynamicelements.no>
2020-06-18 16:26:41 +0530

    Bug fix for message mover.

--- ServiceBusSessionQueue/SimpleMessageMover/Program.cs
+++ ServiceBusSessionQueue/SimpleMessageMover/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
 using System;
 using System.Threading.Tasks;
 
@@ -12,34 +13,33 @@ namespace SimpleMessageMover
         /* ***********************************************************************/
         /* Move messages from source session queue to destination session queue  */
         /* ***********************************************************************/
-
         private static int ReadCount = 0;
         private static int WriteCount = 0;
+        private static IMessageSender senderClient;
+        private static QueueClient readerClient;
 
         static void Main(string[] args)
         {
             try
-            {   
-                // TODO - Update connection string before you run this console app
-                var sourceQueueConnectionString = "Endpoint=sb://entitypath.servicebus.windows.net/;SharedAccessKeyName=Listen;SharedAccessKey=XXXXXXX";
-                var sourceQueueEntityPath = "entitypathSource";
-                var destQueueConnectionString = "Endpoint=sb://entitypath.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=XXXXXXX";
-                var destQueueEntityPath = "entitypathDestination";
-               
+            {
+                // TODO - Update connection string before you run this console app 
+                var sourceQueueConnectionString = "Endpoint=sb://sessionqueuetest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=6mUVVSe/NMWHkuGWbGu4oa81sw+0Eooq1hO84NMrWoE=";
+                var sourceQueueEntityPath = "SourceQ";
+                var destQueueConnectionString = "Endpoint=sb://sessionqueuetest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=6mUVVSe/NMWHkuGWbGu4oa81sw+0Eooq1hO84NMrWoE=";
+                var destQueueEntityPath = "DestQ";
+
                 // Create Queue client for source queue (From which we need to read the messages)
-                QueueClient readerClient = new QueueClient(sourceQueueConnectionString, sourceQueueEntityPath, ReceiveMode.PeekLock);
+                readerClient = new QueueClient(sourceQueueConnectionString, sourceQueueEntityPath, ReceiveMode.PeekLock);
 
                 // Create message sender client for destination queue (To which we need to write/move message)
-                MessageSender senderClient = new MessageSender(destQueueConnectionString, destQueueEntityPath);
+                senderClient = new MessageSender(destQueueConnectionString, destQueueEntityPath);
 
                 Console.WriteLine("Queue Client Creation Done.");
 
                 // Start Listener for source queue and write messages into destination queue.
-                ReadMessageFromSourceSessionQueue(readerClient, senderClient);
+                ReadMessageFromSourceSessionQueue();
                 Console.ReadKey();
 
-                // Close reader
-                readerClient.CloseAsync();
             }
             catch (Exception ex)
             {
@@ -47,22 +47,22 @@ namespace SimpleMessageMover
             }
         }
 
-        static void ReadMessageFromSourceSessionQueue(QueueClient readerClient, MessageSender senderClient)
+        static void ReadMessageFromSourceSessionQueue()
         {
             // Register Session Handler and send the message
             readerClient.RegisterSessionHandler(
                    async (session, message, cancellationToken) =>
                    {
-                       ReadCount = ReadCount + 1;                       
+                       ReadCount = ReadCount + 1;
                        lock (Console.Out)
                        {
                            Console.ForegroundColor = ConsoleColor.Cyan;
                            Console.WriteLine("Message read: Session {0}, MessageId = {1}, ReadCount = {2}",
                                message.SessionId, message.MessageId, ReadCount);
                            Console.ResetColor();
-
-                           ProcessMessageAsync(message, senderClient);
                        }
+
+                       await ProcessMessageAsync(message);
 
                        // Delete message from source queue (reader queue)
                        await session.CompleteAsync(message.SystemProperties.LockToken);
@@ -77,15 +77,22 @@ namespace SimpleMessageMover
                     });
         }
 
-        static void ProcessMessageAsync(Message message, MessageSender senderClient)
+        static async Task ProcessMessageAsync(Message message)
         {
-            senderClient.SendAsync(message);
+            // Create new message from existing one
+            Message messageCopy = new Message();
+            messageCopy.Body = message.Body;
+            messageCopy.SessionId = message.SessionId;
+            messageCopy.MessageId = message.MessageId;
+
+            await senderClient.SendAsync(messageCopy);
+
             lock (Console.Out)
             {
                 WriteCount = WriteCount + 1;
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Message sent: Session {0}, MessageId = {1}, WriteCount = {2}",
-                    message.SessionId, message.MessageId, WriteCount);
+                    messageCopy.SessionId, messageCopy.MessageId, WriteCount);
                 Console.ResetColor();
             }
         }
